d9ac21d2dfc121777ffa8f1ef4ecc477407eb214
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2025-07-13 16:23:17 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -1,4 +1,4 @@
-# (c) adarsh-goel (c) @biisal
+ # (c) adarsh-goel (c) @biisal
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -18,7 +18,7 @@ class Var(object):
     name = str(getenv('name', 'File_Press_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001720210775'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001910391568'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001720210775'))
     PORT = int(getenv('PORT', '9097'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
